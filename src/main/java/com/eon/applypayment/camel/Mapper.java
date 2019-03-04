bb34cf19657573b94637d0ebd12d0501ce1b2439
Ankamma Pallapu(ankamma.pallapu) <ankamma.pallapu@infosys.com>
2019-03-04 12:50:40 +0530

    done

--- src/main/java/com/eon/applypayment/camel/Mapper.java
+++ src/main/java/com/eon/applypayment/camel/Mapper.java
@@ -1,72 +1,94 @@
 package com.eon.applypayment.camel;
 
 import java.io.IOException;
+import java.util.Random;
 
+import org.joda.time.LocalDate;
 import org.springframework.stereotype.Component;
 
 import com.eon.applypayment.util.ApplyPaymentStatus;
 import com.eon.applypayment.util.Utilz;
 import com.eon.applypayment.vo.ApplyPaymentStatusEvent;
+import com.eon.applypayment.vo.ApplyPaymentStatusEvents;
 import com.eon.applypayment.vo.PostBalanceRequest;
 import com.eon.applypayment.vo.PostUtrnRequest;
 import com.eon.applypayment.vo.TransactionDetail;
 import com.eon.applypayment.vo.UtrnAppliedConfirmationRequest;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class Mapper {
 
-	public ApplyPaymentStatusEvent mapPaymentUtrnRequested(TransactionDetail transactionDetail)
-			throws JsonParseException, JsonMappingException, IOException{
+	public String mapPaymentUtrnRequested(TransactionDetail transactionDetail)
+			throws JsonParseException, JsonMappingException, IOException {
+		ApplyPaymentStatusEvents applyPaymentStatusEvents = new ApplyPaymentStatusEvents();
 		ApplyPaymentStatusEvent applyPaymentStatusEvent = new ApplyPaymentStatusEvent();
 		applyPaymentStatusEvent.setEventDateTime(Utilz.getCurrentDate());
 		applyPaymentStatusEvent.setEventType(ApplyPaymentStatus.PaymentUtrnRequested.toString());
 		applyPaymentStatusEvent.setPaygProductId(transactionDetail.getPaygProductId());
 		applyPaymentStatusEvent.setReason("Payment");
-		applyPaymentStatusEvent.setValue(""+transactionDetail.getValue());
-		return applyPaymentStatusEvent;
+		applyPaymentStatusEvent.setTransactionId(getTransactionId());
+		applyPaymentStatusEvent.setValue("" + transactionDetail.getValue());
+		applyPaymentStatusEvents.setId(getId(transactionDetail.getPaygProductId()));
+		applyPaymentStatusEvents.setApplyPaymentStatusEvent(applyPaymentStatusEvent);
+		ObjectMapper Obj = new ObjectMapper();
+
+		return Obj.writeValueAsString(applyPaymentStatusEvents);
+
 	}
-	
-	public ApplyPaymentStatusEvent mapUtrnReceived(PostUtrnRequest postUtrnRequest)
-			throws JsonParseException, JsonMappingException, IOException{
+
+	public String mapUtrnReceived(PostUtrnRequest postUtrnRequest)
+			throws JsonParseException, JsonMappingException, IOException {
+		ApplyPaymentStatusEvents applyPaymentStatusEvents = new ApplyPaymentStatusEvents();
 		ApplyPaymentStatusEvent applyPaymentStatusEvent = new ApplyPaymentStatusEvent();
 		applyPaymentStatusEvent.setEventDateTime(Utilz.getCurrentDate());
 		applyPaymentStatusEvent.setEventType(ApplyPaymentStatus.UtrnReceived.toString());
 		applyPaymentStatusEvent.setPaygProductId(postUtrnRequest.getPaygProductId());
 		applyPaymentStatusEvent.setReason("Payment");
 		applyPaymentStatusEvent.setUtrn(postUtrnRequest.getUtrn());
-		applyPaymentStatusEvent.setValue(""+postUtrnRequest.getValue());
-		return applyPaymentStatusEvent;
+		applyPaymentStatusEvent.setValue("" + postUtrnRequest.getValue());
+		applyPaymentStatusEvents.setId(getId(postUtrnRequest.getPaygProductId()));
+		applyPaymentStatusEvents.setApplyPaymentStatusEvent(applyPaymentStatusEvent);
+		ObjectMapper Obj = new ObjectMapper();
+		return Obj.writeValueAsString(applyPaymentStatusEvents);
 	}
-	
-	public ApplyPaymentStatusEvent mapApplyPaymentUtrnRequested(PostUtrnRequest postUtrnRequest)
-			throws JsonParseException, JsonMappingException, IOException{
+
+	public String mapApplyPaymentUtrnRequested(PostUtrnRequest postUtrnRequest)
+			throws JsonParseException, JsonMappingException, IOException {
+		ApplyPaymentStatusEvents applyPaymentStatusEvents = new ApplyPaymentStatusEvents();
 		ApplyPaymentStatusEvent applyPaymentStatusEvent = new ApplyPaymentStatusEvent();
 		applyPaymentStatusEvent.setEventDateTime(Utilz.getCurrentDate());
 		applyPaymentStatusEvent.setEventType(ApplyPaymentStatus.ApplyUtrnRequested.toString());
 		applyPaymentStatusEvent.setPaygProductId(postUtrnRequest.getPaygProductId());
 		applyPaymentStatusEvent.setReason("Payment");
 		applyPaymentStatusEvent.setUtrn(postUtrnRequest.getUtrn());
-		return applyPaymentStatusEvent;
+		applyPaymentStatusEvents.setId(getId(postUtrnRequest.getPaygProductId()));
+		applyPaymentStatusEvents.setApplyPaymentStatusEvent(applyPaymentStatusEvent);
+		ObjectMapper Obj = new ObjectMapper();
+		return Obj.writeValueAsString(applyPaymentStatusEvents);
 	}
-	
-	
-	
-	public ApplyPaymentStatusEvent mapUtrnApplied(UtrnAppliedConfirmationRequest utrnAppliedConfirmation)
-			throws JsonParseException, JsonMappingException, IOException{
+
+	public String mapUtrnApplied(UtrnAppliedConfirmationRequest utrnAppliedConfirmation)
+			throws JsonParseException, JsonMappingException, IOException {
+		ApplyPaymentStatusEvents applyPaymentStatusEvents = new ApplyPaymentStatusEvents();
 		ApplyPaymentStatusEvent applyPaymentStatusEvent = new ApplyPaymentStatusEvent();
 		applyPaymentStatusEvent.setEventDateTime(Utilz.getCurrentDate());
 		applyPaymentStatusEvent.setEventType(ApplyPaymentStatus.UtrnApplied.toString());
 		applyPaymentStatusEvent.setPaygProductId(utrnAppliedConfirmation.getPaygProductId());
 		applyPaymentStatusEvent.setReason("Payment");
 		applyPaymentStatusEvent.setUtrn(utrnAppliedConfirmation.getUtrn());
-		applyPaymentStatusEvent.setValue(""+utrnAppliedConfirmation.getValue());
-		return applyPaymentStatusEvent;
+		applyPaymentStatusEvent.setValue("" + utrnAppliedConfirmation.getValue());
+		applyPaymentStatusEvents.setId(getId(utrnAppliedConfirmation.getPaygProductId()));
+		applyPaymentStatusEvents.setApplyPaymentStatusEvent(applyPaymentStatusEvent);
+		ObjectMapper Obj = new ObjectMapper();
+		return Obj.writeValueAsString(applyPaymentStatusEvents);
 	}
-	
-	public ApplyPaymentStatusEvent mapPostMeterBalance(PostBalanceRequest postBalanceRequest)
-			throws JsonParseException, JsonMappingException, IOException{
+
+	public String mapPostMeterBalance(PostBalanceRequest postBalanceRequest)
+			throws JsonParseException, JsonMappingException, IOException {
+		ApplyPaymentStatusEvents applyPaymentStatusEvents = new ApplyPaymentStatusEvents();
 		ApplyPaymentStatusEvent applyPaymentStatusEvent = new ApplyPaymentStatusEvent();
 		applyPaymentStatusEvent.setEventDateTime(Utilz.getCurrentDate());
 		applyPaymentStatusEvent.setEventType(ApplyPaymentStatus.MeterBalanceReceived.toString());
@@ -75,8 +97,25 @@ public class Mapper {
 		applyPaymentStatusEvent.setMeterBalanceDateTime(postBalanceRequest.getMeterBalanceDate());
 		applyPaymentStatusEvent.setMeterBalance(postBalanceRequest.getMeterBalance());
 		applyPaymentStatusEvent.setTransactionId(postBalanceRequest.getTransactionId());
-		return applyPaymentStatusEvent;
+		applyPaymentStatusEvents.setId(getId(postBalanceRequest.getPaygProductId()));
+		applyPaymentStatusEvents.setApplyPaymentStatusEvent(applyPaymentStatusEvent);
+		ObjectMapper Obj = new ObjectMapper();
+		return Obj.writeValueAsString(applyPaymentStatusEvents);
 	}
-	
+
+	private String getId(String paygProductId) {
+		int year = LocalDate.now().getYear();
+		int month = LocalDate.now().getMonthOfYear();
+		String id = new StringBuffer().append(paygProductId).append("::").append("E").append("::").append(year)
+				.append("::").append(month).toString();
+
+		return id;
+	}
+	public static String getTransactionId() {
+		Random rnd = new Random();
+		int number = rnd.nextInt(9999999);
+		return String.format("%07d", number);
+	}
+
 //	public mapGetUtrnResponseTo
 }
--- src/main/java/com/eon/applypayment/rest/ApplyPaymentController.java
+++ src/main/java/com/eon/applypayment/rest/ApplyPaymentController.java
@@ -3,9 +3,12 @@ package com.eon.applypayment.rest;
 import javax.validation.Valid;
 
 import org.apache.camel.ProducerTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,43 +19,45 @@ import com.eon.applypayment.vo.TransactionDetail;
 import com.eon.applypayment.vo.UtrnAppliedConfirmationRequest;
 
 @RestController
-//@RequestMapping("/payments")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ApplyPaymentController {
-
+	private static final Logger logger = LoggerFactory.getLogger(ApplyPaymentController.class);
 	@Autowired
 	ProducerTemplate producerTemplate;
 
-	@PostMapping(consumes = "application/json", path="/payments")
-	public ResponseEntity<HttpStatus> applyPayment(
-			@Valid @RequestBody TransactionDetail transactionDetail) {
-		System.out.println(transactionDetail);
+	@PostMapping(consumes = "application/json", path = "/payments")
+	public ResponseEntity<HttpStatus> applyPayment(@Valid @RequestBody TransactionDetail transactionDetail) {
+		logger.info("ApplyPaymentController class of applyPayment method start :{}", transactionDetail.toString());
 		producerTemplate.sendBody("direct:applyPayment", transactionDetail);
 		return new ResponseEntity<HttpStatus>(HttpStatus.ACCEPTED);
 
 	}
 
-	@PostMapping(consumes = "application/json", path="/utrns")
-	public ResponseEntity<HttpStatus> postUtrn(
-			@Valid @RequestBody PostUtrnRequest postUtrnRequest) {
+	@PostMapping(consumes = "application/json", path = "/utrns")
+	public ResponseEntity<HttpStatus> postUtrn(@Valid @RequestBody PostUtrnRequest postUtrnRequest) {
+		logger.info("ApplyPaymentController class of postUtrn method start :{}", postUtrnRequest.toString());
 		producerTemplate.sendBody("direct:handleUtrnPosted", postUtrnRequest);
 		return new ResponseEntity<HttpStatus>(HttpStatus.ACCEPTED);
 
 	}
 
-	@PostMapping(consumes = "application/json", path="/utrnapplied")
+	@PostMapping(consumes = "application/json", path = "/utrnapplied")
 	public ResponseEntity<HttpStatus> handleUtrnAppliedConfirmation(
 			@Valid @RequestBody UtrnAppliedConfirmationRequest utrnAppliedConfirmationRequest) {
+		logger.info("ApplyPaymentController class of handleUtrnAppliedConfirmation method start :{}",
+				utrnAppliedConfirmationRequest.toString());
 		producerTemplate.sendBody("direct:handleUtrnApplied", utrnAppliedConfirmationRequest);
 		return new ResponseEntity<HttpStatus>(HttpStatus.ACCEPTED);
 
 	}
-	
-	@PostMapping(consumes = "application/json", path="/meterbalance")
-	public ResponseEntity<HttpStatus> postMeterBalance(
-			@Valid @RequestBody PostBalanceRequest postBalanceRequest) {
+
+	@PostMapping(consumes = "application/json", path = "/meterbalance")
+	public ResponseEntity<HttpStatus> postMeterBalance(@Valid @RequestBody PostBalanceRequest postBalanceRequest) {
+		logger.info("ApplyPaymentController class of handleUtrnAppliedConfirmation method start :{}",
+				postBalanceRequest.toString());
 		producerTemplate.sendBody("direct:handleMeterBalancePosted", postBalanceRequest);
 		return new ResponseEntity<HttpStatus>(HttpStatus.ACCEPTED);
 
 	}
-	
+
 }
